0d9e94b8badb79bb4d62e1621c8dba16bd55b74f
pablosGomes <104365552+pablosGomes@users.noreply.github.com>
2023-01-09 14:47:31 +0000

    depuração

--- screens/Ride.js
+++ screens/Ride.js
@@ -16,7 +16,7 @@ export default class TransactionScreen extends Component {
 
   getCameraPermissions = async domState => {
 
-    //const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
     //const { status } = Permissions.askAsync(Permissions.CAMERA);
     //const { status } = await Permissions.askAsync(Permissions);
     //const { status } = await Permissions.askAsync(CAMERA);
@@ -56,39 +56,14 @@ export default class TransactionScreen extends Component {
           {hasCameraPermissions ? scannedData : "Solicitar Permissão para a Câmera"}
         </Text>
 
-        {/*
-        <TouchableOpacity
-          style={[styles.button, { marginTop: 25 }]}
-          onPress= this.getCameraPermissions("scanner")}
-        >
-          <Text style={styles.buttonText}>Digitalizar QR Code</Text>
-        </TouchableOpacity>
-        */}
-
-        {/*
+        
         <TouchableOpacity
           style={[styles.button, { marginTop: 25 }]}
           onPress={() => this.getCameraPermissions("scanner")}
         >
           <Text style={styles.buttonText}>Digitalizar QR Code</Text>
         </TouchableOpacity>
-        */}
-
-        {/*
-        <TouchableOpacity
-          style={[styles.button, { marginTop: 25 }]}
-          onPress={() => this.getCameraPermissions()}
-        >
-          <Text style={styles.buttonText}>Digitalizar QR Code</Text>
-        </TouchableOpacity>
-        */}
-
-        {/*
-        <TouchableOpacity
-          style={[styles.button, { marginTop: 25 }]}
-          onPress={() => this.getCameraPermissions("scanner")}
-        >
-        */}
+        
 
       </View>
     );
